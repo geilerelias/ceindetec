5d072a16c75ee40b3f0d1bd54525d1df8a1f801d
Geiler Elias Radillo Sarmiento <geilerelias@gmail.com>
2022-10-14 00:00:02 -0500

    Correccion de rutas para la visualizacion de imagenes

--- resources/js/Pages/AddInformation.vue
+++ resources/js/Pages/AddInformation.vue
@@ -102,7 +102,7 @@
                                         <v-list-item
                                             :key="person.id"
                                         >
-                                            {{ person.profile_photo_path }}
+                                            
                                             <v-list-item-avatar>
                                                 <img class="rounded-circle" cover
                                                      :src="person.profile_photo_path==='' ||person.profile_photo_path===null?`https://ui-avatars.com/api/?name=${person.name+person.surname}&color=7F9CF5&background=EBF4FF`:'/storage/'+person.profile_photo_path"></img>
